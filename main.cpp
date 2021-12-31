88bdcba5cd90a6e373ed7e34efc71cc1fed4d4e4
alx <alx.yulax@gmail.com>
2021-12-31 17:18:55 +1200

    1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <map>
+
+class Number {
+    std::string number;
+};
+
+class Contact {
+    std::string name;
+    std::vector<Number *> numbers;
+
+};
+
+class AdressBook {
+    //std::map<std::string, Contact*> contact
+
+};
+
+class Mobile {
+
+
+};
 
 int main() {
-    std::cout << "Hello, World!1" << std::endl;
+
     return 0;
 }
